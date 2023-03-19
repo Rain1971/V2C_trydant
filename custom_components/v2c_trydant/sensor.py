a4c985e38d3ee8852bfa366827dff9f294ba4e78
rain1971 <rafa@tarracoimportexport.com>
2023-03-19 12:41:56 +0100

    primera version funcional. Falta arreglar hardcode de la IP

--- custom_components/v2c_trydant/sensor.py
+++ custom_components/v2c_trydant/sensor.py
@@ -1,59 +1,68 @@
+import asyncio
 import logging
 import aiohttp
 import async_timeout
-from datetime import timedelta
+from typing import Callable, Any
+import datetime
 
-from homeassistant.helpers.entity import Entity
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed, CoordinatorEntity
+from homeassistant.core import HomeAssistant
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.const import CONF_IP_ADDRESS
+from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+    DataUpdateCoordinator,
+    UpdateFailed,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
 async def async_get_json(session, url):
     async with async_timeout.timeout(30):
         async with session.get(url) as response:
-            return await response.json()
+            return await response.json(content_type=None)
 
-class V2C_trydantData:
-    def __init__(self, ip_address):
-        self._ip_address = ip_address
-        self.attributes = {}
+async def async_setup_platform(hass: HomeAssistant, config: dict, async_add_entities: Callable[[list], Any], discovery_info=None):
+    #ip_address = config.get(CONF_IP_ADDRESS)
+    ip_address = "10.48.130.141"
 
-    async def async_update(self):
-        url = f"http://{self._ip_address}/RealTimeData"
+    async def async_update_data():
+        _LOGGER.debug(f"Fetching data from {ip_address}")
         async with aiohttp.ClientSession() as session:
             try:
-                data = await async_get_json(session, url)
-                self.attributes = data
-                _LOGGER.debug(f"Fetched data from {url}: {data}")
+                data = await async_get_json(session, f"http://{ip_address}/RealTimeData")
+                _LOGGER.debug(f"Received data: {data}")
+                return data
             except Exception as e:
-                raise UpdateFailed(f"Error fetching data from {url}: {e}")
-
-class V2C_trydantSensor(CoordinatorEntity, Entity):
-    def __init__(self, coordinator, attribute):
-        super().__init__(coordinator)
-        self._attribute = attribute
-
-    @property
-    def name(self):
-        return f"V2C_trydant {self._attribute}"
-
-    @property
-    def state(self):
-        return self.coordinator.data.attributes.get(self._attribute)
-
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    _LOGGER.debug("Setting up V2C_trydant platform")
-    ip_address = hass.data[DOMAIN][CONF_IP_ADDRESS]
-    charger_data = V2C_trydantData(ip_address)
+                raise UpdateFailed(f"Error fetching data from {ip_address}: {e}")
 
     coordinator = DataUpdateCoordinator(
         hass,
         _LOGGER,
-        name="V2C_trydant",
-        update_method=charger_data.async_update,
-        update_interval=timedelta(seconds=60),
+        name="V2C_trydant_sensor",
+        update_method=async_update_data,
+        update_interval=datetime.timedelta(seconds=30),
     )
 
     await coordinator.async_config_entry_first_refresh()
 
-    async_add_entities([V2C_trydantSensor(coordinator, attr) for attr in charger_data.attributes.keys()])
+    async_add_entities([V2C_trydantSensor(coordinator, attr) for attr in coordinator.data.keys()], True)
+
+class V2C_trydantSensor(CoordinatorEntity, SensorEntity):
+    def __init__(self, coordinator, attribute):
+        super().__init__(coordinator)
+        self._attribute = attribute
+        self._attr_name = f"V2C_trydant_{attribute}"
+        self._attr_unique_id = f"v2c_trydant_{attribute}"
+
+    @property
+    def name(self):
+        return self._attr_name
+
+    @property
+    def unique_id(self):
+        return self._attr_unique_id
+
+    @property
+    def state(self):
+        return self.coordinator.data.get(self._attribute)
