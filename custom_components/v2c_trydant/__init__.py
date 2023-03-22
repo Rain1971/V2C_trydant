c9f0db04c929c449653ba33e0204274bfd493d13
rain1971 <rafa@tarracoimportexport.com>
2023-03-22 09:51:01 +0100

    write all numeric

--- custom_components/v2c_trydant/__init__.py
+++ custom_components/v2c_trydant/__init__.py
@@ -1,10 +1,16 @@
 """The v2c_trydant component."""
 from homeassistant.config_entries import SOURCE_IMPORT
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, ServiceCall
+from homeassistant.const import CONF_IP_ADDRESS
+import logging
+import aiohttp
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+
+_LOGGER = logging.getLogger(__name__)
 
 DOMAIN = "v2c_trydant"
 
-PLATFORMS = ["sensor", "switch"]
+PLATFORMS = ["sensor", "switch", "number"]
 
 async def async_setup(hass: HomeAssistant, config: dict):
     hass.data.setdefault(DOMAIN, {})
@@ -25,11 +31,74 @@ async def async_setup_entry(hass: HomeAssistant, entry):
     for platform in PLATFORMS:
         await hass.config_entries.async_forward_entry_setup(entry, platform)
 
+    hass.data[DOMAIN]["ip_address"] = entry.data[CONF_IP_ADDRESS]
+    ip_address = entry.data[CONF_IP_ADDRESS]
+
+    async def set_min_intensity(call: ServiceCall):
+        min_intensity = call.data.get("min_intensity")
+        if 6 <= int(min_intensity) <= 32:
+            await async_set_min_intensity(hass, ip_address, min_intensity)
+        else:
+            _LOGGER.error("min_intensity must be between 6 and 32")
+
+    async def set_max_intensity(call):
+        max_intensity = call.data["max_intensity"]
+        if 6 <= int(max_intensity) <= 32:
+            await async_set_max_intensity(hass, ip_address, max_intensity)
+        else:
+            _LOGGER.error("max_intensity must be between 6 and 32")
+
+    async def set_min_intensity_slider(call):
+        min_intensity = call.data.get("v2c_min_intensity")
+        if min_intensity is not None:
+            if 6 <= int(min_intensity) <= 32:
+                if entry:
+                    ip_address = entry.data[CONF_IP_ADDRESS]
+                    await async_set_min_intensity(hass, ip_address, min_intensity)
+                else:
+                    _LOGGER.error(f"Entity ID not found: {entity_id}")
+            else:
+                _LOGGER.error("v2c_min_intensity must be between 6 and 32")
+        else:
+            _LOGGER.error("v2c_max_intensity not provided")
+
+    async def set_max_intensity_slider(call):
+        max_intensity = call.data.get("v2c_max_intensity")
+        if max_intensity is not None:
+            if 6 <= int(max_intensity) <= 32:
+                if entry:
+                    ip_address = entry.data[CONF_IP_ADDRESS]
+                    await async_set_max_intensity(hass, ip_address, max_intensity)
+                else:
+                    _LOGGER.error(f"Entity ID not found: {entity_id}")
+            else:
+                _LOGGER.error("v2c_max_intensity must be between 6 and 32")
+        else:
+            _LOGGER.error("v2c_max_intensity not provided")
+
+    hass.services.async_register(DOMAIN, "set_min_intensity", set_min_intensity)
+    hass.services.async_register(DOMAIN, "set_max_intensity", set_max_intensity)
+    hass.services.async_register(DOMAIN, "set_min_intensity_slider", set_min_intensity_slider)
+    hass.services.async_register(DOMAIN, "set_max_intensity_slider", set_max_intensity_slider)
+
+
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry):
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
 
+async def async_set_min_intensity(hass: HomeAssistant, ip_address: str, min_intensity: int):
+    _LOGGER.debug(f"Setting min intensity to {min_intensity}")
+    async with aiohttp.ClientSession() as session:
+        url = f"http://{ip_address}/write/MinIntensity={min_intensity}"
+        async with session.get(url) as response:
+            response.raise_for_status()
 
+async def async_set_max_intensity(hass, ip_address: str, max_intensity):
+    _LOGGER.debug(f"Setting max intensity to {max_intensity}")
+    async with aiohttp.ClientSession() as session:
+        url = f"http://{ip_address}/write/MaxIntensity={max_intensity}"
+        async with session.get(url) as response:
+            response.raise_for_status()
 
---
+++ custom_components/v2c_trydant/number.py
@@ -0,0 +1,87 @@
+from homeassistant.components.number import NumberEntity
+from homeassistant.const import DEVICE_DEFAULT_NAME
+from homeassistant.helpers import config_validation as cv
+import logging
+from . import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    async_add_entities([MaxIntensityNumber(hass)])
+    async_add_entities([MinIntensityNumber(hass)])
+
+class MaxIntensityNumber(NumberEntity):
+    def __init__(self, hass):
+        self._hass = hass
+        self._state = 32
+
+    @property
+    def name(self):
+        return "v2c_max_intensity"
+
+    @property
+    def icon(self):
+        return "mdi:car"
+
+    @property
+    def native_unit_of_measurement(self):
+        return "A"
+
+    @property
+    def native_value(self):
+        return self._state
+
+    @property
+    def native_max_value(self):
+        return 32
+
+    @property
+    def native_min_value(self):
+        return 6
+
+    async def async_set_native_value(self, value):
+        if 6 <= value <= 32:
+            ip_address = self._hass.data[DOMAIN]["ip_address"]
+            await self._hass.services.async_call(DOMAIN, "set_max_intensity_slider", {"v2c_max_intensity": value})
+            self._state = value
+            self.async_write_ha_state()
+        else:
+            _LOGGER.error("v2c_max_intensity must be between 6 and 32")
+
+class MinIntensityNumber(NumberEntity):
+    def __init__(self, hass):
+        self._hass = hass
+        self._state = 6
+
+    @property
+    def name(self):
+        return "v2c_min_intensity"
+
+    @property
+    def icon(self):
+        return "mdi:car"
+
+    @property
+    def native_unit_of_measurement(self):
+        return "A"
+
+    @property
+    def native_value(self):
+        return self._state
+
+    @property
+    def native_max_value(self):
+        return 32
+
+    @property
+    def native_min_value(self):
+        return 6
+
+    async def async_set_native_value(self, value):
+        if 6 <= value <= 32:
+            ip_address = self._hass.data[DOMAIN]["ip_address"]
+            await self._hass.services.async_call(DOMAIN, "set_min_intensity_slider", {"v2c_min_intensity": value})
+            self._state = value
+            self.async_write_ha_state()
+        else:
+            _LOGGER.error("v2c_min_intensity must be between 6 and 32")
