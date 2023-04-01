44992a1b2caedc72c6b67a9e044a4f9c48def953
Rafael Salas <rafa@tarracoimportexport.com>
2023-04-01 13:27:12 +0200

    sustituyo error por debug

--- custom_components/v2c_trydan/coordinator.py
+++ custom_components/v2c_trydan/coordinator.py
@@ -13,6 +13,7 @@ _LOGGER = logging.getLogger(__name__)
 class V2CtrydanDataUpdateCoordinator(DataUpdateCoordinator):
     def __init__(self, hass, ip_address):
         self.ip_address = ip_address
+        self.error_reported = False
 
         super().__init__(hass, logging.getLogger("null"), name="v2c_trydan", update_interval=timedelta(seconds=2))
 
@@ -20,9 +21,15 @@ class V2CtrydanDataUpdateCoordinator(DataUpdateCoordinator):
         try:
             async with aiohttp.ClientSession() as session:
                 data = await self._async_get_json(session, f"http://{self.ip_address}/RealTimeData")
+                self.error_reported = False
                 return data
         except Exception as e:
-            raise UpdateFailed(f"Error fetching data from {self.ip_address}: {e}")
+            if not self.error_reported:
+                self.error_reported = True
+                raise UpdateFailed(f"Error fetching data from {self.ip_address}: {e}")
+            else:
+                _LOGGER.debug(f"Error fetching data from {self.ip_address}: {e}")
+                raise UpdateFailed("Error communicating with API")
 
     async def _async_get_json(self, session, url):
         try:
@@ -30,8 +37,18 @@ class V2CtrydanDataUpdateCoordinator(DataUpdateCoordinator):
                 response.raise_for_status()
                 return await response.json(content_type=None)
         except aiohttp.ClientError as err:
-            _LOGGER.error(f"Error communicating with API: {err}")
-            raise UpdateFailed("Error communicating with API")
+            if not self.error_reported:
+                self.error_reported = True
+                _LOGGER.error(f"Error communicating with API: {err}")
+                raise UpdateFailed("Error communicating with API")
+            else:
+                _LOGGER.debug(f"Error communicating with API: {err}")
+                raise UpdateFailed("Error communicating with API")
         except Exception as e:
-            _LOGGER.error(f"Error parsing JSON data: {e}")
-            raise UpdateFailed("Error parsing JSON data")
+            if not self.error_reported:
+                self.error_reported = True
+                _LOGGER.error(f"Error parsing JSON data: {e}")
+                raise UpdateFailed("Error parsing JSON data")
+            else:
+                _LOGGER.debug(f"Error parsing JSON data: {e}")
+                raise UpdateFailed("Error parsing JSON data")
